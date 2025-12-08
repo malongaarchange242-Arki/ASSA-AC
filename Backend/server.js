52c483e7e90729ee87285408f46aa79a224650b5
malongaarchange242-Arki <malongaarchaange242@gmail.com>
2025-12-08 08:25:33 -0700

    Résolution conflits

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,6 @@ const allowedOrigins = [
   'http://localhost:5500',
   'http://127.0.0.1:5500',
   'https://assa-ac.onrender.com',
-  'https://assa-ac.netlify.app',
   'https://assa-ac-test.netlify.app'
 ];
 
