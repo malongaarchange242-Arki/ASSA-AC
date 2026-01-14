baa00034bb5f73bbe8e8ee13d554cf9e3b22a9a2
malongaarchange242-Arki <malongaarchaange242@gmail.com>
2026-01-14 14:32:04 -0700

    fix(frontend): normalize line endings and update UI logic

--- Backend/server.js
+++ Backend/server.js
@@ -44,7 +44,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5500',
   'https://assa-ac-jyn4.onrender.com',
   'https://assa-ac.onrender.com',
-  'https://assa-ac.netlify.app',
+  'https://https://assa-ac-cemac.netlify.app/',
   'https://assa-ac-test.netlify.app'
 ];
 
